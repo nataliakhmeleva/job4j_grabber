43fef8c9e92a637121722f034f6cba9c79ab4bc5
nataliakhmeleva <nataliya-mar@mail.ru>
2022-06-25 10:08:09 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #315963]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -15,8 +15,8 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 5; i++) {
-            Connection connection = Jsoup.connect(PAGE_LINK);
+        for (int i = 1; i < 6; i++) {
+            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + i);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
