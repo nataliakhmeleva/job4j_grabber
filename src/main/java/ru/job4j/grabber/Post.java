5055a6a1124f032e05a561c47b8dd8ca93764468
nataliakhmeleva <nataliya-mar@mail.ru>
2022-06-25 10:16:49 +0300

    2.2. Модель данных - Post. [#285211 #315995]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -67,12 +67,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description) && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description, created);
+        return Objects.hash(id, link);
     }
 
     @Override
